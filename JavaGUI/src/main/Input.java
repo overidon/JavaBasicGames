18896297b9c7b87765d45122617a78a9cbf2cf6c
Tyler Stansfield Jaggers <32966867+overidon@users.noreply.github.com>
2020-10-17 12:08:24 -0500

    Create Input.java
    this will integrate with the test project ... make sure to connect to your view controller.

---
+++ JavaGUI/src/main/Input.java
@@ -0,0 +1,79 @@
+public class Input implements KeyListener, MouseMotionListener, MouseInputListener, Elements {
+	
+	private DrawWindow  dw; 
+	
+	public Input (DrawWindow dw) {
+		this.dw = dw;
+		
+
+		dw.addKeyListener(this);
+		dw.addMouseMotionListener(this);
+		dw.addMouseListener(this);
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		
+		char code = e.getKeyChar();
+
+
+				
+	// end of the keyPressed method 	
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+		char code = e.getKeyChar();
+
+
+
+	// end of the keyReleased method
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+
+
+
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		System.out.println("Click");
+
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+
+	}
+}
